5bd0aec674e54a52f3501da3892ef2cbf46d8c6d
Mbina <117299384+mobinaqabeigii@users.noreply.github.com>
2023-01-10 18:53:16 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+const int  n=5;
+     main(){
+	 
+	   int a[n];
+	   int b[n];
+	   int i ,j ,temp;
+	   
+	   cout<<"enter"<<n<<"elements:";
+	   for(i=0; i<n; i++) {
+	   	cin>>a[i];
+	   }
+       cout<<"enter"<<n<<"element:";
+    	for(j=0; j<n; j++){
+    		cin>>b[i];
+    		
+			}
+			for(i=0; i<n; i++)
+			cout<<""<<a[i];
+			cout<<"next"<<endl;
+			for(i=0; i<n; i++)
+			cout<<""<<b[i];
+}
